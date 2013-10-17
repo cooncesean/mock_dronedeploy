a42fd21a0a979b3d79f03c14b8853c495cd70885
cooncesean <cooncesean@gmail.com>
2013-10-17 11:53:16 -0700

    Removing debug comment.

--- application.py
+++ application.py
@@ -9,7 +9,5 @@ def auth():
     " Render the app 'allowance' page. "
     return render_template('auth.html')
 
-# https://api.instagram.com/oauth/authorize/?client_id=2e7067c2b22a4cbdb6b2e0e89cbd6537&response_type=code&redirect_uri=http://data.ink361.com/v1/auth?instagram=1
-
 if __name__ == '__main__':
     application.run()
