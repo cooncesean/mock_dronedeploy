5b0d473dba98a84ea6164712cdfc5096f3bddb94
cooncesean <cooncesean@gmail.com>
2013-10-17 11:50:57 -0700

    Adding static, and templates. Rendering the mock oauth page.

--- application.py
+++ application.py
@@ -1,10 +1,13 @@
 import flask
 
+from flask import render_template
+
 application = flask.Flask(__name__)
 
 @application.route('/oauth/authorize/')
-def authentication():
-    return "Hello world!"
+def auth():
+    " Render the app 'allowance' page. "
+    return render_template('auth.html')
 
 # https://api.instagram.com/oauth/authorize/?client_id=2e7067c2b22a4cbdb6b2e0e89cbd6537&response_type=code&redirect_uri=http://data.ink361.com/v1/auth?instagram=1
 
